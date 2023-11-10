f48056958ac35cdd6be27923f417d28d522b98ff
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-11-10 16:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var isDate = function (input) {
   //   write your code here
+	if (input instanceof Date) {
+    return !isNaN(input);
+  }
+
+  // Check if the input is a string that can be parsed into a Date
+  if (typeof input === 'string') {
+    const parsedDate = new Date(input);
+    return !isNaN(parsedDate);
+  }
+
+  return false;
 };
 
 // Do not change the code below.
