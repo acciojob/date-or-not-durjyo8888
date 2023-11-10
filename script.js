ebc5d8017cad0974a8b91c12be08d3d03eb54e14
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-11-10 17:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var isDate = function (input) {
   //   write your code here
-	if (input instanceof Date) {
-    return !isNaN(input);
+	 if (input instanceof Date && !isNaN(input)) {
+    return true;
   }
 
-  // Check if the input is a string that can be parsed into a Date
+  // Check if it's a valid date string
   if (typeof input === 'string') {
     const parsedDate = new Date(input);
     return !isNaN(parsedDate);
+  }
+
+  // Check for UNIX timestamp
+  if (typeof input === 'number') {
+    const dateFromTimestamp = new Date(input);
+    return !isNaN(dateFromTimestamp);
   }
 
   return false;
